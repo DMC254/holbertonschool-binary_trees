95e583b04c33c113bd59f317061f41546e59f58d
DMC254 <mercadodiego2006@gmail.com>
2024-12-12 09:11:18 -0400

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,14 +8,19 @@
  * Return: If node is NULL or has no uncle, NULL.
  *         Otherwise, a pointer to the uncle node.
  */
-binary_tree_t
-*binary_tree_uncle(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL ||
 		node->parent == NULL ||
 		node->parent->parent == NULL)
-			return (NULL);
+	{
+		return (NULL);
+	}
+
 	if (node->parent->parent->left == node->parent)
+	{
 		return (node->parent->parent->right);
+	}
+
 	return (node->parent->parent->left);
 }
