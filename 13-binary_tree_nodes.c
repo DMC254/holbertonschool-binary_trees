e2ce534f97b3c29e718ba0e64382f08f0be01dd7
DMC254 <mercadodiego2006@gmail.com>
2024-12-12 08:44:14 -0400

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,7 +7,7 @@
  * Return: If tree is NULL, the function must return 0, else return node count.
  */
 
-size_t binary_tree_node(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
@@ -15,7 +15,7 @@ size_t binary_tree_node(const binary_tree_t *tree)
 	{
 		nodes += (tree->left || tree->right) ? 1 : 0;
 		nodes += binary_tree_nodes(tree->left);
-		nodes += binary_tree_nodes (tree->right);
+		nodes += binary_tree_nodes(tree->right);
 	}
 	return (nodes);
 }
